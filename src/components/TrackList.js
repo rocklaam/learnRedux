6a74db160de2df92b54f93994f5872839de71c9a
lam.nguyen <lam.nguyen@sioux.asia>
2018-07-25 18:23:58 +0700

    init commit

---
+++ src/components/TrackList.js
@@ -0,0 +1,31 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+
+class TrackList extends Component {
+    static propTypes = {
+        tracks: PropTypes.array
+    }
+
+    static defaultProps = {
+        tracks: []
+    }
+
+    render() {
+        return (
+            <div>
+                {
+                    this.props.tracks.map((track, key) => {
+                        return <div key={key}> Track:{track.title} </div>;
+                    })
+                }
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    const tracks = state.tracks
+    return { tracks }
+}
+
+export default connect(mapStateToProps)(TrackList);
