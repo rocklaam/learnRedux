23def063cf3a89c87dbb8d964529dc117ee34023
rocklaam <Lam.Nguyen@sioux.asia>
2019-07-29 15:33:38 +0700

    Add a config to enable to see Redux store in debug

--- src/store.js
+++ src/store.js
@@ -7,5 +7,6 @@ const logger = createLogger();
 const createStoreWithMiddleware = applyMiddleware(logger)(createStore);
 
 export function configureStore(initialState) {
-    return createStoreWithMiddleware(rootReducer, initialState);
+    return createStoreWithMiddleware(rootReducer, initialState,
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 }
